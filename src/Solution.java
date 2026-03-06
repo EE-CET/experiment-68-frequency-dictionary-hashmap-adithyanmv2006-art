37a79c267fa51317bd33d8380a7f422a0357cf67
Adithyan <adithyanmv2006@gmail.com>
2026-03-06 09:53:13 +0530

    code added

--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,19 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        // TODO: Read a single line of text
-        
-        // TODO: Split the text into individual words
-        
-        // TODO: Create a HashMap to store the frequency of each word
-        
-        // TODO: Iterate through the words and update their frequencies in the map
-        
-        // TODO: Iterate through the map and print the unique words and their counts
-        // Format: "word: count"
-        
+        Scanner sc = new Scanner(System.in);
+        LinkedHashMap<String,Integer> wordCount = new LinkedHashMap<>();
+
+        String line = sc.nextLine();
+        String[] words = line.split(" ");
+
+        for(String word : words){
+            wordCount.put(word, wordCount.getOrDefault(word,0)+1);
+        }
+
+        for(Map.Entry<String,Integer> entry : wordCount.entrySet()){
+            System.out.println(entry.getKey()+": "+entry.getValue());
+        }
     }
 }
