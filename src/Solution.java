d8aeae039b7be58f37d9aad6346d877bac22c28d
Adithyan <adithyanmv2006@gmail.com>
2026-03-06 10:23:43 +0530

    3rd commit

--- src/Solution.java
+++ src/Solution.java
@@ -1,26 +1,21 @@
-import java.util.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        LinkedHashMap<String,Integer> wordCount = new LinkedHashMap<>();
 
         String line = sc.nextLine();
         String[] words = line.split(" ");
 
-        HashMap<String, Integer> map = new HashMap<>();
-
-        for (String word : words) {
-            if (map.containsKey(word)) {
-                map.put(word, map.get(word) + 1);
-            } else {
-                map.put(word, 1);
-            }
+        for(String word : words){
+            wordCount.put(word, wordCount.getOrDefault(word,0)+1);
         }
 
-        for (String key : map.keySet()) {
-            System.out.println(key + ": " + map.get(key));
+        for(Map.Entry<String,Integer> entry : wordCount.entrySet()){
+            System.out.println(entry.getKey()+": "+entry.getValue());
         }
-
-        sc.close();
     }
 }
